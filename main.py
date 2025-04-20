46dff726f54d5af6c751f0ceaac6443cd0121834
kitanaya <seb.heinzmann@gmail.com>
2025-04-20 07:51:35 +0200

    added fps clock

--- main.py
+++ main.py
@@ -13,12 +13,18 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    fps = pygame.time.Clock()
+    dt = 0
+
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
+        dt = fps.tick(60) / 1000
+        
+
 
 
 if __name__ == "__main__":
