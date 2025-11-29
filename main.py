be2d4e403f996c710b51f9a0d26669fef3405b65
kitanaya <seb.heinzmann@gmail.com>
2025-11-29 08:17:29 +0100

    test123

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from shot import Shot
 
 
 def main():
-    print("Starting Asteroids!")
+    print("Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
